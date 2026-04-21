511c7833ee2b536e646fd34bc767be1867f1715a
Kikiler <kikilercool@gmail.com>
2026-04-21 12:46:12 +0200

    fixed visual bugs involving the navbar not displaying which links you currently are in

--- header/header.js
+++ header/header.js
@@ -1,6 +1,51 @@
 function createHeader() {
-  const header = document.createElement('header');
-  header.innerHTML = `
+  const header = document.createElement("header");
+  if (document.title.includes("Home")) {
+    header.innerHTML = `
+    <nav class="nav">
+      <a href="/" class="nav-brand">ESET <span>/ Student Racing</span></a>
+      <ul class="nav-links">
+        <li><a class = "active" href="/">Home</a></li>
+        <li><a href="/about/about.html">About</a></li>
+        <li><a href="/sponsors/sponsors.html">Sponsors</a></li>
+        <li><a href="/join/join.html">Join</a></li>
+      </ul>
+    </nav>`;
+  } else if (document.title.includes("About")) {
+    header.innerHTML = `
+    <nav class="nav">
+      <a href="/" class="nav-brand">ESET <span>/ Student Racing</span></a>
+      <ul class="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a class="active" href="/about/about.html">About</a></li>
+        <li><a href="/sponsors/sponsors.html">Sponsors</a></li>
+        <li><a href="/join/join.html">Join</a></li>
+      </ul>
+    </nav>`;
+  } else if (document.title.includes("Sponsors")) {
+    header.innerHTML = `
+    <nav class="nav">
+      <a href="/" class="nav-brand">ESET <span>/ Student Racing</span></a>
+      <ul class="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about/about.html">About</a></li>
+        <li><a class="active" href="/sponsors/sponsors.html">Sponsors</a></li>
+        <li><a href="/join/join.html">Join</a></li>
+      </ul>
+    </nav>`;
+  } else if (document.title.includes("Join")) {
+    header.innerHTML = `
+    <nav class="nav">
+      <a href="/" class="nav-brand">ESET <span>/ Student Racing</span></a>
+      <ul class="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about/about.html">About</a></li>
+        <li><a href="/sponsors/sponsors.html">Sponsors</a></li>
+        <li><a class="active" href="/join/join.html">Join</a></li>
+      </ul>
+    </nav>`;
+  } else {
+    header.innerHTML = `
     <nav class="nav">
       <a href="/" class="nav-brand">ESET <span>/ Student Racing</span></a>
       <ul class="nav-links">
@@ -9,8 +54,9 @@ function createHeader() {
         <li><a href="/sponsors/sponsors.html">Sponsors</a></li>
         <li><a href="/join/join.html">Join</a></li>
       </ul>
-    </nav>
-  `;
+    </nav>`;
+  }
+
   document.body.insertAdjacentElement("afterbegin", header);
 }
 
