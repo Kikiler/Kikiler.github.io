bd6fd8eb8d45c559ad17a9e42e4c28e07f48d65e
Kikiler <kikilercool@gmail.com>
2026-08-16 00:14:39 +0300

    added links to instagram and linkedin newly created pages.

--- footer/footer.js
+++ footer/footer.js
@@ -1,7 +1,11 @@
 function createFooter() {
   const footer = document.createElement('footer');
   footer.innerHTML = `
-    <p>© ECAM Solar Endurance Team — ESET</p>
+    <div class="footer-div">
+    <a href="https://www.linkedin.com/eset" class="linkedinLogo"> LinkedInLogo </a>
+    <a href="https://www.instagram.com/eset_ecam" class="instaLogo"> InstagramLogo </a> 
+    <span>© ECAM Solar Endurance Team — ESET</span>
+    </div>
   `;
   document.body.appendChild(footer);
 }
