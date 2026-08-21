d35f33f8ea8bd8f8b7849dbd29a74af4c59283c2
Kikiler <kikilercool@gmail.com>
2026-08-21 22:07:55 +0200

    added correct links to the socials + logos

--- footer/footer.js
+++ footer/footer.js
@@ -2,9 +2,9 @@ function createFooter() {
   const footer = document.createElement('footer');
   footer.innerHTML = `
     <div class="footer-div">
-    <a href="https://www.linkedin.com/eset" class="linkedinLogo"> LinkedInLogo </a>
-    <a href="https://www.instagram.com/eset_ecam" class="instaLogo"> InstagramLogo </a> 
+    <a href="https://www.linkedin.com/company/eset-ecam-solar-endurance-team"> <img class="linkedinLogo" src="/ressources/LIlogo.png"/></a>
     <span>© ECAM Solar Endurance Team — ESET</span>
+    <a href="https://www.instagram.com/eset_ecam/"> <img class="instaLogo" src="/ressources/Instalogo.png"/> </a> 
     </div>
   `;
   document.body.appendChild(footer);
